801fd59850355a9af44b494697f29dc718b102b1
Simon Zhong <simon@kx1qfm6f52.lan>
2024-05-06 20:26:36 -0700

    add logging and Sentry config

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,34 @@
 package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import io.sentry.SendCachedEnvelopeFireAndForgetIntegration;
+import io.sentry.SendFireAndForgetEnvelopeSender;
 import io.sentry.Sentry;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
+        Sentry.init(options -> {
+            options.setEnableExternalConfiguration(true);
+            options.setCacheDirPath("/Users/simon/Projects/my-log4j2/disk/path");
+            options.addIntegration(
+                new SendCachedEnvelopeFireAndForgetIntegration(
+                new SendFireAndForgetEnvelopeSender(options::getCacheDirPath)
+                )
+            );
+            options.setConnectionTimeoutMillis(10000);
+            options.setReadTimeoutMillis(10000);
+        });
+
         System.out.println("Hello and welcome!");
-        logger.info("Info level log message");
-        logger.error("Error level log message1");
+        logger.debug("Debug message");
+        logger.info("Info message");
+        logger.warn("Warn message");
+        logger.error("Error message");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
             logger.info("i = " + i);
         }
@@ -25,5 +38,8 @@ public class Main {
         } catch (Exception e) {
             Sentry.captureException(e);
         }
+
+        int example = 1 / 0;
+
     }
 }
